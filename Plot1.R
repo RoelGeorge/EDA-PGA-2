930486001666b5bb9c43580ea55462fe7a2b2fca
RoelGeorge <roelgeorge@gmail.com>
2020-08-21 01:28:57 +0530

    Adding Comments

--- Plot1.R
+++ Plot1.R
@@ -1,7 +1,11 @@
- NEI <- readRDS("summarySCC_PM25.rds")
+### reading the two datasets 
+NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
+### aggregating the data
 EmissionPerYear<-aggregate(Emissions~year,NEI,sum)
+### creating the plot1.png file 
 png(filename = "plot1.png")
+### Creating the plot
 barplot( height =  EmissionPerYear$Emissions,
          names.arg = EmissionPerYear$year,
          xlab = "Years",ylab = "total PM2.5 emission",
