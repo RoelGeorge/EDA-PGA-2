0fc541ce9fa53bcd2f3b40394956a4baac059a8b
RoelGeorge <roelgeorge@gmail.com>
2020-08-20 22:32:20 +0530

    Corrections

--- Plot1.R
+++ Plot1.R
@@ -1,5 +1,9 @@
  NEI <- readRDS("summarySCC_PM25.rds")
 SCC <- readRDS("Source_Classification_Code.rds")
 EmissionPerYear<-aggregate(Emissions~year,NEI,sum)
-barplot(EmissionPerYear$year,EmissionPerYear$Emissions,
-        xlab = "Years",ylab = "Emission(in ton)")
+png(filename = "plot1.png")
+barplot( height =  EmissionPerYear$Emissions,
+         names.arg = EmissionPerYear$year,
+         xlab = "Years",ylab = "total PM2.5 emission",
+         main="total PM2.5 emission from all sources for each of the years")
+dev.off()
