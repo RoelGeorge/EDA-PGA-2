75a27265047c29dd5d7a0196fcb808ff096fe193
RoelGeorge <roelgeorge@gmail.com>
2020-08-20 22:06:38 +0530

    reading and plotting data

---
+++ Plot1.R
@@ -0,0 +1,5 @@
+ NEI <- readRDS("summarySCC_PM25.rds")
+SCC <- readRDS("Source_Classification_Code.rds")
+aggregate(Emissions~year,NEI,sum)
+barplot(EmissionPerYear$year,EmissionPerYear$Emissions,
+        xlab = "Years",ylab = "Emission(in ton)")
